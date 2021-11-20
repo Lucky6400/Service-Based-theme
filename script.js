a91a34ce089200511dc569c0c6762b7912c2d36f
Lucky6400 <94078735+Lucky6400@users.noreply.github.com>
2021-11-20 21:55:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+ $(document).ready(function(){
+    $('#card1').mouseenter(function() {
+        $('#card1').css({'background':'royalblue', 'color':'white'}) 
+     });
+     $('#card1').mouseleave(function() {
+        $('#card1').css({'background':'white', 'color':'black'}) 
+     });
+     $('#card2').mouseenter(function() {
+        $('#card2').css({'background':'royalblue', 'color':'white'}) 
+     });
+     $('#card2').mouseleave(function() {
+        $('#card2').css({'background':'white', 'color':'black'}) 
+     });
+     $('#card3').mouseenter(function() {
+        $('#card3').css({'background':'royalblue', 'color':'white'}) 
+     });
+     $('#card3').mouseleave(function() {
+        $('#card3').css({'background':'white', 'color':'black'}) 
+     });
+     $('#card4').mouseenter(function() {
+        $('#card4').css({'background':'royalblue', 'color':'white'}) 
+     });
+     $('#card4').mouseleave(function() {
+        $('#card4').css({'background':'white', 'color':'black'}) 
+     });
+ });
